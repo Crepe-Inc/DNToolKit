22721d103158dee3c7284765fe64ea5339dc3036
TheLostTree <65834918+TheLostTree@users.noreply.github.com>
2022-09-08 21:11:09 -0700

    dont console.log the giant 45 megabyte log that you're about to download pls

--- Frontend/src/lib/DownloadLog.svelte
+++ Frontend/src/lib/DownloadLog.svelte
@@ -6,8 +6,8 @@
     function getLog(){
         let c = log.join("█▄█\n");
         var element = document.createElement('a');
-        console.log(c)
-        console.log(log)
+        // console.log(c)
+        // console.log(log)
         element.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(c));
 
         element.setAttribute('download', "Log.dntkap");
